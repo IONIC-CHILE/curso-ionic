04419714a256319dfb8390a9d4102789988784d5
Marlon Falcon Hernandez <falconsoft.3d@gmail.com>
2017-01-08 01:05:55 -0300

    4
    4

---
+++ listViewWeb/www/js/controllers.js
@@ -0,0 +1,25 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope,$http) {
+
+ $scope.data=[];
+
+ $http.get('https://www.reddit.com/r/ios/.json')
+ .success(function(response){
+     
+       console.log(response);
+
+     angular.forEach(response.data.children,function(child){
+
+         $scope.data.push(child.data);
+
+      }
+     
+
+      );
+
+ });
+   
+
+})
+;
