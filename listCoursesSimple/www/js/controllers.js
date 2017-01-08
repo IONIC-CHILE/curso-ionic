503a4301380e79961e230b9e9d6dccada8e73283
Marlon Falcon Hernandez <falconsoft.3d@gmail.com>
2017-01-07 23:00:03 -0300

    Ejercicio 2 Lista

---
+++ listCoursesSimple/www/js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope) {
+
+$scope.cursos=[
+  
+  {
+    nombre: 'Node JS',
+    descripcion:'Para principiantes'
+  },
+
+  {
+    nombre: 'Delphi',
+    descripcion:'Para principiantes universitarios de Chile, , voluptate natus voluptates ullam aspernatur ratione, alias.'
+  },
+
+
+  {
+    nombre: 'HTML',
+    descripcion:'Para principiantes'
+  },
+
+    {
+    nombre: 'Google',
+    descripcion:'Para principiantes'
+  }
+
+];
+
+
+})
+
+;
